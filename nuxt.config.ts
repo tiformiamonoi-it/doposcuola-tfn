35dd62cc5de12c027c12b365ceb8821fce439c35
AceisX <alessandrobaronem@gmail.com>
2026-06-13 00:03:58 +0200

    feat: configure Nuxt 4 with Nuxt UI, Google Fonts, app/ directory

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: '2024-11-01',
+
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/google-fonts',
+  ],
+
+  googleFonts: {
+    families: {
+      Montserrat: [600, 700],
+      Inter: [400, 500, 600],
+    },
+    display: 'swap',
+    preload: true,
+  },
+
+  css: ['~/assets/css/main.css'],
+})
